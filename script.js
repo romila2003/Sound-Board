e3d8c41fb3e5678f5fb9a9e7fdb76b6eb4c14b99
romila2003 <93338535+romila2003@users.noreply.github.com>
2022-08-14 13:21:07 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const sounds = ["👏 " + "applause", "😠 " + "boo", "😨 " + "gasp", "🥳 " +  "tada", "🏆 " + "victory", "❌ " + "wrong"];
+
+sounds.forEach(sound => {
+    const btn = document.createElement("button");
+    btn.classList.add("btn");
+
+    btn.innerText = sound;
+
+    btn.addEventListener("click", () => {
+        stopSongs();
+
+        document.getElementById(sound).play();
+    })
+
+    document.getElementById("buttons").appendChild(btn);
+})
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound);
+
+        song.pause();
+        song.currentTime = 0;
+    })
+}
